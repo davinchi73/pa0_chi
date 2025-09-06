d691ebe3876e69e923bb64c82d0a8abd5134200f
Devinn Chi <70714199+davinchi73@users.noreply.github.com>
2025-09-06 17:29:26 -0400

    Completed Const (hopefully) created var

---
+++ pa0/autograd/var.py
@@ -0,0 +1,24 @@
+# SYSTEM IMPORTS
+from typing import Type
+from expr import Expression, ExpressionType
+
+
+class Constant(Expression):
+    def __init__(self: ExpressionType, val: float) -> None:
+            self.val = float(val)
+
+    def differentiate(self: ExpressionType) -> ExpressionType:
+        return Constant(0.0)
+
+    def eval(self: ExpressionType,
+             x: float) -> float:
+        return self.val
+
+    def deepcopy(self: ExpressionType) -> ExpressionType:
+        return Constant(self.val)
+    
+    def __str__(self):
+        return str(self.val)
+    
+    def __repr__(self):
+        return f"Constant:({self.val})"
