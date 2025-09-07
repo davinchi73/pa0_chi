ee7f2dd32d6773139c9ed6f7f7e893ac5a4e5ca1
Devinn Chi <70714199+davinchi73@users.noreply.github.com>
2025-09-06 21:56:28 -0400

    exp method complete
    kinda

---
+++ pa0/autograd/exp.py
@@ -0,0 +1,28 @@
+# SYSTEM IMPORTS
+from typing import Type
+from expr import Expression, ExpressionType
+from binop import BinaryOp, Op
+import math
+
+class Exp(Expression):
+    def __init__(self: ExpressionType, arg: Expression) -> None:
+        self.arg = arg
+
+    def __str__(self):
+        arg = str(self.arg)
+        return f"exp({arg})"
+    
+    def __repr__(self):
+        arg = str(self.arg)
+        return f"exp expression: Exp({arg})"
+
+    def differentiate(self: ExpressionType) -> ExpressionType:
+        ddx = self.arg.differentiate()
+        return BinaryOp(ddx, Op.MUL, Exp(self.arg))
+
+    def eval(self: ExpressionType,
+             x: float) -> float:
+        return math.exp(self.arg.eval(x))
+
+    def deepcopy(self: ExpressionType) -> ExpressionType:
+        return Exp(self.arg.deepcopy())
