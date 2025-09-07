9753bb0bf0948d5184f02fb6dca8697bb2d9c622
Devinn Chi <70714199+davinchi73@users.noreply.github.com>
2025-09-06 21:52:26 -0400

    log done, exp created (im whippping these out bruh)

---
+++ pa0/autograd/log.py
@@ -0,0 +1,28 @@
+# SYSTEM IMPORTS
+from typing import Type
+from expr import Expression, ExpressionType
+from binop import BinaryOp, Op
+import math
+
+class Log(Expression):
+    def __init__(self: ExpressionType, arg: Expression) -> None:
+        self.arg = arg
+
+    def __str__(self):
+        arg = str(self.arg)
+        return f"log({arg})"
+    
+    def __repr__(self):
+        arg = str(self.arg)
+        return f"log expression: Log({arg})"
+
+    def differentiate(self: ExpressionType) -> ExpressionType:
+        ddx = self.arg.differentiate()
+        return BinaryOp(ddx, Op.DIV, self.arg)
+
+    def eval(self: ExpressionType,
+             x: float) -> float:
+        return math.ln(self.arg.eval(x))
+
+    def deepcopy(self: ExpressionType) -> ExpressionType:
+        return Log(self.arg.deepcopy())
